503a94b65014e27f391903c0f4035be9e9b436eb
Anuj <anuj@Admins-MacBook-Pro.local>
2020-02-12 12:09:12 +0530

    sample jenkins:

---
+++ HelloJenkins.java
@@ -0,0 +1,19 @@
+package code.jenkins.jenkins;
+
+public class HelloJenkins {
+
+    public static void main(String[] args) {
+        
+        // int num1, num2, sum;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter First Number: ");
+        // num1 = sc.nextInt();
+        
+        // System.out.println("Enter Second Number: ");
+        // num2 = sc.nextInt();
+        
+        // sc.close();
+        int sum = 1 + 5;
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}
